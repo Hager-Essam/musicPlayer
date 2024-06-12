7d261e078627a2fef6b3743ef039da1e6b050735
Hager <hager1.essam@gmail.com>
2024-06-12 09:53:59 +0200

    first Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let musicName = document.getElementsByClassName("musicName");
+let musicImage = document.getElementsByClassName("musicImage");
+let previousSong = document.getElementsByClassName("previousSong");
+let nextSong = document.getElementsByClassName("nextSong");
+let currentSong = document.getElementsByClassName("CurrentSong");
+let volume = document.getElementsByClassName("volume");
+let songSlider = document.getElementsByClassName("songSlider");
+
+let musicList = [
+    {
+        songName: "Lovely",
+        image: "images/Billi.jpg",
+         songPath: "music/lovely.m4a"
+    },
+    {
+        songName: "omry",
+        image: "images/cc6ed5abaa9855f9b812c2a7f82d0a9b.jpg",
+        songPath: "music/OmKolthom.m4a"
+    },
+    {
+        songName: "senorita",
+        image:"images/R.jpg",
+        songPath:"music/Shawn Mendes, Camila Cabello - Señorita (Lyrics).m4a"
+    },
+];
+
